ab6097e8c31f9fdf1f4b80430a7b79b64c41d454
cemim <filipe.cemim@gmail.com>
2021-07-06 20:00:57 -0300

    Aula Debug de Código

--- java_basico/caracteristicas-da-linguagem-java-2/src/main/java/strings/Strings.java
+++ java_basico/caracteristicas-da-linguagem-java-2/src/main/java/strings/Strings.java
@@ -11,7 +11,7 @@ public class Strings {
     System.out.println(nome);
     System.out.println("Nome do cliente : " + nome);
     System.out.println("Nome completo do cliente : " + nomeCompleto);
-    var string = new String(" Minha  String ");
+    var string = new String(" Minha String ");
 
     System.out.println("Char na posição : " + string.charAt(5));
     System.out.println("Quantidade=" + string.length());
