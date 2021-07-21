275031edafdd03301c8698c7f68aa0cf1a948789
cemim <filipe.cemim@gmail.com>
2021-07-20 22:20:51 -0300

    Inicio da Aula de Spring Boot

---
+++ introducao_springboot/src/main/java/com/fcemim/introducao_springboot/IntroducaoSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.fcemim.introducao_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroducaoSpringbootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(IntroducaoSpringbootApplication.class, args);
+	}
+
+}
---
+++ introducao_springboot/src/test/java/com/fcemim/introducao_springboot/IntroducaoSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcemim.introducao_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroducaoSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
