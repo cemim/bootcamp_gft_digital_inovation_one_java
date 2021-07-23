33519c523a3c56f4a60d414eab3b3b8b7d4f3cf9
cemim <filipe.cemim@gmail.com>
2021-07-23 14:07:45 -0300

    Aula Introdução ao SpringBoot

--- introducao_springboot/src/main/java/com/fcemim/introducao_springboot/controller/AppController.java
+++ introducao_springboot/src/main/java/com/fcemim/introducao_springboot/controller/AppController.java
@@ -1,13 +1,17 @@
 package com.fcemim.introducao_springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class AppController {
+
+    @Value("${app.message}")
+    private String appMessage;
 
     @GetMapping("/")
-    public String helloMessage(){
-        return "Hello, Digital Inovation One";
+    public String getAppMessage(){
+        return appMessage;
     }
 }
